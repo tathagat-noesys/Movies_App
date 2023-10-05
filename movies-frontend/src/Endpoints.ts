fc4272525db9baa78a6a04656817d59cd280c6c6
Tathagat <tathagatraj4@gmail.com>
2023-10-05 10:33:51 +0530

    env configured

---
+++ movies-frontend/src/Endpoints.ts
@@ -0,0 +1,2 @@
+const baseURL = import.meta.env.VITE_REACT_APP_API_URL;
+export const URLgenres = `${baseURL}/genres`;
--- movies-frontend/src/Genres/IndexGenres.tsx
+++ movies-frontend/src/Genres/IndexGenres.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
+import { genreDTO } from "./genres.model";
+import { URLgenres } from "../Endpoints";
 
 const IndexGenres = () => {
+  useEffect(() => {
+    console.log(URLgenres);
+    axios
+      .get(URLgenres)
+      .then((res: AxiosResponse<genreDTO[]>) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
       <h3>Genres</h3>
